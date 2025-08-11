fdc1c518ffa903c067bf1fa72077ed3b64cac5c7
“Anas <anaskhan2209k@gmail.com>
2025-08-11 19:05:55 +0530

    Create React implimentation

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'static'),
+    filename: 'bundle.js',
+  },
+  module: {
+  rules: [
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          targets: "defaults",
+          presets: [
+            ['@babel/preset-react']
+          ]
+        }
+      }
+    }
+  ]
+}
+};
